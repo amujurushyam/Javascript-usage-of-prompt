4f77c7294869f8b026b1b56b55f3cf79117d28eb
amujurushyam <amujurushyam@gmail.com>
2024-12-18 18:15:15 +0530

    prompt

---
+++ prac.js
@@ -0,0 +1,12 @@
+
+    let username=prompt("Enter username")
+        let obj=
+        {
+            name:username,
+            product:"washingmachine",
+            price:25000,
+            day:"monday",
+            occassion:"pongal",
+            platform:"amazon"
+        }
+        alert(`${obj.name} has purchased a ${obj.product} which cost  ${obj.price} will be delivery on ${obj.day} on the occassion of  ${obj.occassion} in ${obj.platform}`)
